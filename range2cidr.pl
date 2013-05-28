305fb6d2be0e61b75666ac1c6c36cd3189e44d77
Kim Lidström <dxtr+github@dxtr.cc>
2013-05-28 14:46:25 +0200

    First commit!
    Added the script!

---
+++ range2cidr.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use List::MoreUtils qw(uniq);
+use Net::CIDR;
+use Net::CIDR ':all';
+
+my @tmp = qw();
+
+foreach my $line (<STDIN>) {
+        chomp($line);
+        if (length($line) && $line !~ /^\s*#/ && $line =~ /:((\d{1,3}[-\.]*){8})/) {
+                push(@tmp, $1);
+        }
+}
+
+my @result = sort(uniq(Net::CIDR::range2cidr(@tmp)));
+
+print join("\n", @result);
+print "\n";
+
