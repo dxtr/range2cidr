19d581e7d4b5d09ee3c26a7d59f3faca3d11e186
Kim Lidström <kim.lidas+github@gmail.com>
2013-05-30 09:59:04 +0200

    Removed List::MoreUtils as a dependency

--- range2cidr.pl
+++ range2cidr.pl
@@ -2,21 +2,22 @@
 
 use utf8;
 use strict;
-use List::MoreUtils qw(uniq);
-use Net::CIDR;
 use Net::CIDR ':all';
 
 my @tmp = qw();
 
 foreach my $line (<STDIN>) {
-        chomp($line);
-        if (length($line) && $line !~ /^\s*#/ && $line =~ /(.*:)?((\d{1,3}[-\.]*){8})/) {
-                push(@tmp, $2);
-        }
+	chomp($line);
+	if (length($line) && $line !~ /^\s*#/ && $line =~
+		/(.*:)?((\d{1,3}[-\.]*){8})$/) {
+		push(@tmp, $2);
+	}
 }
 
-my @result = sort(uniq(Net::CIDR::range2cidr(@tmp)));
+my @result = do {
+	my %seen = ();
+	grep { not $seen{$_}++ } Net::CIDR::range2cidr(@tmp);
+};
 
 print join("\n", @result);
 print "\n";
-
